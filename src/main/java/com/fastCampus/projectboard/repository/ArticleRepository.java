d21f8a6b70cfc80b6f2462fd7b488b7b3be32b45
LeeGangEun <rkddms123456@gmail.com>
2022-08-01 22:12:38 +0900

    .

--- src/main/java/com/fastCampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastCampus/projectboard/repository/ArticleRepository.java
@@ -1,7 +1,31 @@
 package com.fastCampus.projectboard.repository;
 
 import com.fastCampus.projectboard.domain.Article;
+import com.fastCampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Repository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+@RepositoryRestResource
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.hashtag, root.createdBy, root.createdAt);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
 }
